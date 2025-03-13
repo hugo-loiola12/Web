2f97fa4bf1bab1f3ac040885f743dfe86aaed649
Hugo Loiola <hugoloiola.1@gmail.com>
2025-03-13 14:14:39 -0300

    soma

--- index.ts
+++ index.ts
@@ -4,3 +4,12 @@ const prompt = promptSync();
 const nome = prompt("Qual o seu nome? ");
 
 console.log(`Olá, ${nome}!`);
+
+let a  = 10;
+let b  = 20;
+
+function soma(a: number, b: number): number {
+    return a + b;
+}
+
+console.log(`A soma de ${a} e ${b} é ${soma(a, b)}`);

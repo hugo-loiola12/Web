e6de5a1fc4e4c4bcc3eaed2a7be0eac6ff41c335
Hugo Loiola <hugoloiola.1@gmail.com>
2025-03-25 17:40:25 -0300

    feat: yarn 4

--- Aula.md
+++
@@ -1,3 +0,0 @@
-- Projeto
-- Wireframe
-- Linguegem
--- index.ts
+++ index.ts
@@ -1,15 +1,4 @@
-import promptSync from 'prompt-sync';
+import promptSync from "prompt-sync";
 const prompt = promptSync();
-
-const nome = prompt("Qual o seu nome? ");
-
-console.log(`Olá, ${nome}!`);
-
-let a  = 10;
-let b  = 20;
-
-function soma(a: number, b: number): number {
-    return a + b;
-}
-
-console.log(`A soma de ${a} e ${b} é ${soma(a, b)}`);
+const nome = prompt("Nome:");
+console.log(nome);
